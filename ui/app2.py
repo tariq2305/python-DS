9c8534912ccd690f70db1790b569ddef3dc5de49
tariq2305 <tariqabdin2@gmail.com>
2024-03-07 19:53:50 +0530

    pandas

--- ui/app2.py
+++ ui/app2.py
@@ -7,11 +7,11 @@ c1,c2=st.columns(2)
 c1.number_input('Number 1',key='num1')
 c2.number_input('Number 2',key='num2')
 
-if st.button('Subtract'):
+if st.button('Addition'):
         num1=st.session_state.num1
         num2=st.session_state.num2
-        ans=num1-num2
-        st.info(f'{num1}-{num2}={ans}')
+        ans=num1+num2
+        st.info(f'{num1}+{num2}={ans}')
 
 
 if st.button('Subtract'):
